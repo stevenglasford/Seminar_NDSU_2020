f66098736d4056c628165013be6a6b99a136ed1c
stevenglasford <stevenglasford@gmail.com>
2020-10-10 17:32:34 -0500

    begin making new data file instead of changing rochj, makes more sense

---
+++ 2.5/data/data_simulation.h
@@ -0,0 +1,29 @@
+//#include "data_basicmodel.h"		//- basic example, FD method [needs data_default.h]
+//#include "data_advancedmodel.h"       //- basic example, FD method using Hnum numerical hamiltonian <LINKED TO> data_basicmodel.h
+//#include "data_FD_vdp.h"		//- Van Der Pol example, FD method [needs data_default.h]
+//#include "data_FD_zermelo.h"	        //- zermelo example, trajectory reconstruction based on minimal time
+//#include "data_FD_zermelo_boundary2.h"//- zermelo example, different boundary conditions, trajectory reconstruction based on minimal time
+//#include "data_FD_zermelo_valuereconstruction.h" //- zermelo example, trajectory reconstruction based on value (and dyn.prog.prin.)
+//#include "data_FD_3d_rot.h"           //- 3d example with exact solution (rotation in x-y plane of circle or of a plane)
+//#include "data_FD_2d_ex1_basic.h"     //- 2D test, eikonal equation, using control discretisation
+//#include "data_FD_2d_ex1_advanced.h"  //- 2D test, eikonal equation, using Hnum function (in general more precise)	<LINKED TO> data_FD_2d_ex1_basic.h 
+//#include "data_FD_1d_advection.h"	//- 2018 - 1D example - advection test with exact solution 		<LINKED TO> data_FD_1d_advection_default.h
+//#include "data_FD_1d_advection_modif.h" //- 2018 - 1D example - advection with exact solution  		<LINKED TO> data_FD_1d_default.h
+//#include "data_FD_2d_advection.h"	//- 2018 - 2D example - advection test with exact solution 		<LINKED TO> data_FD_2d_advection_default.h
+//#include "data_FD_steady.h"	        //- 1D and 2D steady examples with exact solution
+//#include "data_FD_2d_2c.h"            //- 2D two controls 
+//#include "data_FD_2d_2c_traj.h"       //- 2D two controls + trajectory
+//#include "data_FD_2d_obstacletest.h"  //- 2D obstacle and shattering pb test 
+//#include "data_FD_2d_exittimepb.h"    //- texit/tmax test : maximal exit time test problem
+//#include "data_FD_2d_periodic.h"	//- 2D FD max(advection,0) test with periodic boundary conditions
+//#include "data_SL_2d_rot.h"		//- 2D SL scheme
+//#include "data_SL_2d_periodic.h" 	//- 2D SL max(advection,0) test with periodic bound. cond. 
+//#include "data_SL_steady.h" 		//- 2D (and 1D) SL scheme for steady pb
+//#include "data_FD_value.h"		//- 2D+1 test for a minimal value pb (paper "General HJ.." Altarovici, Bokanowski, Zidani 2012)
+#include "data_FD_nd.h"		//- eikonal equation example in prepared for dimension DIM from 2 to 6 	<<LINKED TO> data_FD_nd_default.h
+
+
+
+
+
+
